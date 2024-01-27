baae2025d883bb8fe49a187eb3f8fc0deba6f200
eunchaelyu <hn04133@naver.com>
2024-01-27 19:04:58 +0900

    Initial commit

---
+++ src/main/java/com/sparta/eroomprojectbe/EroomProjectBeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.eroomprojectbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EroomProjectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EroomProjectBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/eroomprojectbe/EroomProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.eroomprojectbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EroomProjectBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
