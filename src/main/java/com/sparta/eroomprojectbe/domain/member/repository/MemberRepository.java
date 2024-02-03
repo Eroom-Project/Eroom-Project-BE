100aa36bd3ff2222ff7d0a13f4860fd1980efd52
kuma0112 <lsj23871@gmail.com>
2024-02-03 22:55:27 +0900

    feat : member entity 관련 enum, repository 클래스

---
+++ src/main/java/com/sparta/eroomprojectbe/domain/member/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.sparta.eroomprojectbe.domain.member.repository;
+
+import com.sparta.eroomprojectbe.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
