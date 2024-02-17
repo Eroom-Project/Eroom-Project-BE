b46a74098d1564fd5c19724a86cc8c79b5bd481d
eunchaelyu <hn04133@naver.com>
2024-02-17 16:21:28 +0900

    feature: 웹소켓 간단한 예제 test

--- src/main/java/com/sparta/eroomprojectbe/global/config/MyHandler.java
+++ src/main/java/com/sparta/eroomprojectbe/global/config/MyHandler.java
@@ -7,18 +7,21 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class MyHandler extends TextWebSocketHandler {
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception { //웹소켓 연결이 수립된 후 이용하는 세션과 메세지
+    //웹소켓 연결이 수립된 후 이용하는 세션과 메세지
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         System.out.println(message);
         System.out.println(message.getPayload());
     }
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception { //연결이 수립된 직후
+    //연결이 수립된 직후
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         System.out.println("afterConnectionEstablished:" + session.toString());
     }
 
     @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception { //연결이 닫힌 직후
+    //연결이 닫힌 직후
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         super.afterConnectionClosed(session, status);
     }
 }
