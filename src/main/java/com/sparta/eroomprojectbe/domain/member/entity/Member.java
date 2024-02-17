326b68df8793699487f8068af27d38f7eb9c70f3
kuma0112 <lsj23871@gmail.com>
2024-02-17 15:30:59 +0900

    refactor : 기본 이미지 url 변경

--- src/main/java/com/sparta/eroomprojectbe/domain/member/entity/Member.java
+++ src/main/java/com/sparta/eroomprojectbe/domain/member/entity/Member.java
@@ -42,7 +42,7 @@ public class Member {
         this.nickname = nickname;
         this.role = MemberRoleEnum.USER;
         this.isSocialMember = false;
-        this.profileImageUrl ="https://files.slack.com/files-pri/T01L2TNGW3T-F06K5JP92N4/group_69.png";
+        this.profileImageUrl ="https://files.slack.com/files-pri/T01L2TNGW3T-F06JYF3GTB8/image.png";
     }
 
     public Member(String kakaoEmail, String password, String nickname, Long kakaoId, String profileImageUrl) {
