d7564bc8196e161d38e189a27da9d895358456c8
mingang211 <a989977@naver.com>
2024-02-17 16:31:22 +0900

    Refactor : S3 이미지 업로드 시 파일명 랜덤생성

--- src/main/java/com/sparta/eroomprojectbe/domain/challenge/service/ImageS3Service.java
+++ src/main/java/com/sparta/eroomprojectbe/domain/challenge/service/ImageS3Service.java
@@ -4,6 +4,7 @@ import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,6 +25,8 @@ public class ImageS3Service {
     public String saveFile(MultipartFile multipartFile) throws IOException {
         // 파일의 원본 이름을 얻어오고 이후 S3버킷에 저장할 때 사용.
         String originalFileName = multipartFile.getOriginalFilename();
+        // 파일명을 랜덤으로 작성
+        String randomFileName = RandomStringUtils.randomAlphanumeric(10) + "_" + originalFileName;
         // ObjectMetadata 객체를 생성해서 S3 객체에 메타데이터를 추가한다. 파일 크기와 형식을 설정하고 이는 S3콘솔에서 확인할 때, 사용됨
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentLength(multipartFile.getSize());
@@ -32,10 +35,10 @@ public class ImageS3Service {
         // bucketName은 업로드될 버킷의 이름이며, originalFileName은 파일이 S3에 저장될 때 사용될 키입니다.
         // multipartFile.getInputStream()은 업로드할 파일의 InputStream을 제공합니다.
         // metadata 객체는 위에서 설정한 메타데이터 정보를 사용합니다.
-        amazonS3.putObject(bucketName, originalFileName, multipartFile.getInputStream(), metadata);
+        amazonS3.putObject(bucketName, randomFileName,multipartFile.getInputStream(), metadata);
         // 업로드가 완료되면 amazonS3.getUrl을 사용하여 업로드된 객체의 URL을 얻고, 이를 문자열로 반환합니다.
         // 이 URL은 업로드된 파일에 대한 고유한 식별자로 사용될 수 있습니다.
-        return URLDecoder.decode(amazonS3.getUrl(bucketName, originalFileName).toString(), "utf-8");
+        return URLDecoder.decode(amazonS3.getUrl(bucketName, randomFileName).toString(), "utf-8");
     }
     // 전체적으로 이 메서드는 주어진 MultipartFile을 AWS S3에 업로드하고, 업로드된 파일의 URL을 반환합니다.
 
