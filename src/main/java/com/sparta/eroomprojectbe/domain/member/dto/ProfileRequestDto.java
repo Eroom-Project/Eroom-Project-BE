3cd1d0e0c53d621eed79dbe6c7cbcf60f62c9279
mingang211 <a989977@naver.com>
2024-02-17 17:35:01 +0900

    Refactor : 마이 페이지 정보 수정기능 수정

--- src/main/java/com/sparta/eroomprojectbe/domain/member/dto/ProfileRequestDto.java
+++ src/main/java/com/sparta/eroomprojectbe/domain/member/dto/ProfileRequestDto.java
@@ -1,11 +1,12 @@
 package com.sparta.eroomprojectbe.domain.member.dto;
 
 import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 public class ProfileRequestDto {
     private String email;
     private String password;
     private String nickname;
-    private String profileImageUrl;
+    private MultipartFile file;
 }
