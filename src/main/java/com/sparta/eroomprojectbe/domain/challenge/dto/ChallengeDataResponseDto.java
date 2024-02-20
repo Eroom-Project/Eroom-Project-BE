68423c830f75d8abc48bb052cfa82b7255643ba4
mingang211 <a989977@naver.com>
2024-02-20 11:32:13 +0900

    Refactor : 선택한 챌린지 조회할때 로그인 유무에 따라 표시할 수 있는 loginMemberid 추가

--- src/main/java/com/sparta/eroomprojectbe/domain/challenge/dto/ChallengeDataResponseDto.java
+++ src/main/java/com/sparta/eroomprojectbe/domain/challenge/dto/ChallengeDataResponseDto.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
  * ChallengeResponseDto를 data라는 객체로 전해주고, 성공여부메시지와, 상태코드를 전달하는 dto
  */
 @Getter
-public class ChallengeDataResponseDto extends BaseChallengeResponseDto<ChallengeResponseDto> {
-    public ChallengeDataResponseDto(ChallengeResponseDto data, String message, HttpStatus status) {
+public class ChallengeDataResponseDto extends BaseChallengeResponseDto<ChallengeLoginResponseDto> {
+    public ChallengeDataResponseDto(ChallengeLoginResponseDto data, String message, HttpStatus status) {
         super(data, message, status);
     }
 }
