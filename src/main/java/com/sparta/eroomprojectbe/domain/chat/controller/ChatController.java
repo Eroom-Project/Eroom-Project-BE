2bbf2960f1b220403248a500aa0d382e2fbef6f0
eunchaelyu <hn04133@naver.com>
2024-02-23 15:12:06 +0900

    feature: 채팅 기능 구현 수정(현재 참여 인원 정보 리스트 제공)

--- src/main/java/com/sparta/eroomprojectbe/domain/chat/controller/ChatController.java
+++ src/main/java/com/sparta/eroomprojectbe/domain/chat/controller/ChatController.java
@@ -1,84 +1,28 @@
 package com.sparta.eroomprojectbe.domain.chat.controller;
 
-import com.sparta.eroomprojectbe.domain.challenge.entity.Challenge;
-import com.sparta.eroomprojectbe.domain.challenge.repository.ChallengeRepository;
-import com.sparta.eroomprojectbe.domain.challenger.entity.Challenger;
-import com.sparta.eroomprojectbe.domain.challenger.repository.ChallengerRepository;
 import com.sparta.eroomprojectbe.domain.chat.entity.ChatMessage;
-import com.sparta.eroomprojectbe.domain.member.entity.Member;
-import com.sparta.eroomprojectbe.domain.member.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.sparta.eroomprojectbe.domain.chat.service.ChatMessageService;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
 
 
 @Controller
 public class ChatController {
-    private final ChallengerRepository challengerRepository;
-    private final ChallengeRepository challengeRepository;
-    private final MemberRepository memberRepository;
 
-    public ChatController(ChallengerRepository challengerRepository, ChallengeRepository challengeRepository, MemberRepository memberRepository) {
-        this.challengerRepository = challengerRepository;
-        this.challengeRepository = challengeRepository;
-        this.memberRepository = memberRepository;
+    private final ChatMessageService chatMessageService;
+
+    public ChatController(ChatMessageService chatMessageService) {
+        this.chatMessageService = chatMessageService;
     }
-
-    @Autowired
-    private SimpMessageSendingOperations messagingTemplate;
 
     @MessageMapping("/chat.sendMessage/{challengeId}")
     public void sendMessage(@Payload ChatMessage chatMessage,
                             @DestinationVariable("challengeId") String challengeId,
                             Message<?> message) {
-
-        // 회원 ID 가져오기
-        String challengeIdString = chatMessage.getChallengeId();
-        String memberIdString = chatMessage.getMemberId();
-
-        if (challengeIdString != null && memberIdString != null) {
-            // String 값을 Long 값으로 변환
-            Long challenge = Long.parseLong(challengeIdString);
-            Long member = Long.parseLong(memberIdString);
-
-            // 챌린지와 회원을 찾음
-            Optional<Challenge> challengeOptional = challengeRepository.findById(challenge);
-            Optional<Member> memberOptional = memberRepository.findById(member);
-
-            // 챌린지와 회원이 존재하는 경우에만 처리
-            if (challengeOptional.isPresent() && memberOptional.isPresent()) {
-                Optional<Challenger> challengerOptional = challengerRepository.findByChallengeAndMember(challengeOptional.get(), memberOptional.get());
-
-                challengerOptional.ifPresent(challenger -> {
-                    // 닉네임 가져오기
-                    String senderNickname = challenger.getMember().getNickname();
-                    chatMessage.setSender(senderNickname);
-
-                    // 프로필 이미지 URL 가져오기
-                    String profileImageUrl = challenger.getMember().getProfileImageUrl();
-                    chatMessage.setProfileImageUrl(profileImageUrl);
-
-                    // WebSocket 세션에 속성 저장
-                    StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
-                    headerAccessor.getSessionAttributes().put("challengeId", challengeId);
-                    headerAccessor.getSessionAttributes().put("nickname", senderNickname);
-
-                    // 메시지 보낸 시간 저장
-                    chatMessage.setTime(LocalDateTime.now());
-                });
-                messagingTemplate.convertAndSend(String.format("/sub/chat/challenge/%s", challengeId), chatMessage);
-            }
-        }
+        chatMessageService.saveMessage(challengeId, chatMessage, message);
     }
 }
 
