66f8bcc0f086bc09af5e27619bdbbddce8daf1c8
eunchaelyu <hn04133@naver.com>
2024-02-24 13:23:55 +0900

    fix: challenger 연관관계 설정(단방향->양방향)

--- src/main/java/com/sparta/eroomprojectbe/domain/challenger/entity/Challenger.java
+++ src/main/java/com/sparta/eroomprojectbe/domain/challenger/entity/Challenger.java
@@ -1,11 +1,15 @@
 package com.sparta.eroomprojectbe.domain.challenger.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.sparta.eroomprojectbe.domain.auth.entity.Auth;
 import com.sparta.eroomprojectbe.domain.challenge.entity.Challenge;
 import com.sparta.eroomprojectbe.domain.member.entity.Member;
 import com.sparta.eroomprojectbe.global.rollenum.ChallengerRole;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -23,6 +27,10 @@ public class Challenger {
     @ManyToOne(cascade = CascadeType.REMOVE)
     @JoinColumn(name = "challenge_id")
     private Challenge challenge;
+
+    @OneToMany(mappedBy = "challenger", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonBackReference
+    private List<Auth> authList;
 
     @Enumerated(EnumType.STRING)
     private ChallengerRole role;
