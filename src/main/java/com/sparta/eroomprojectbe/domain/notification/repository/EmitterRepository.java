6961bf24a9bb9ba89a61a2144d030fa515e48b00
kuma0112 <lsj23871@gmail.com>
2024-02-29 00:24:49 +0900

    refactor : 알림 기본 기능 리팩토링

--- src/main/java/com/sparta/eroomprojectbe/domain/notification/repository/EmitterRepository.java
+++ src/main/java/com/sparta/eroomprojectbe/domain/notification/repository/EmitterRepository.java
@@ -11,17 +11,21 @@ import java.util.concurrent.ConcurrentHashMap;
 @RequiredArgsConstructor
 public class EmitterRepository {
     // 모든 Emitters를 저장하는 ConcurrentHashMap
-    private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+    // userId를 key 값으로, sseEmitter를 밸류 값으로 한다
+    // pirvate final 이어야 하나?
+    // 왜 concurrenthashmap 이지? 그냥 hashmap은 안 될까?
+    private final Map<Long, SseEmitter> emitterHashMap = new ConcurrentHashMap<>();
 
+    // 저장하는 로직 void로 둘까, or SseEmitter로 반환하게 둘까
     public void save(Long id, SseEmitter emitter) {
-        emitters.put(id, emitter);
+        emitterHashMap.put(id, emitter);
     }
 
     public void deleteById(Long id) {
-        emitters.remove(id);
+        emitterHashMap.remove(id);
     }
 
     public SseEmitter get(Long id) {
-        return emitters.get(id);
+        return emitterHashMap.get(id);
     }
 }
--- src/main/java/com/sparta/eroomprojectbe/domain/notification/service/NotificationService.java
+++ src/main/java/com/sparta/eroomprojectbe/domain/notification/service/NotificationService.java
@@ -5,22 +5,26 @@ import com.sparta.eroomprojectbe.domain.notification.dto.IsReadResponseDto;
 import com.sparta.eroomprojectbe.domain.notification.repository.EmitterRepository;
 import com.sparta.eroomprojectbe.domain.notification.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
     // 기본 타임아웃 설정
     private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
+    private final static String NOTIFICATION_NAME = "notify";
 
     private final EmitterRepository emitterRepository;
     private final NotificationRepository notificationRepository;
 
     public SseEmitter subscribe(Member member, Long userId) {
+        // 새로운 SseEmitter를 만든 후 저장하는 메서드인 createEmitter()를 호출한다.
         SseEmitter emitter = createEmitter(userId);
 
         sendToClient(userId, "EventStream Created. [userId=" + userId + "]");
@@ -51,27 +55,38 @@ public class NotificationService {
         sendToClient(userId, event);
     }
 
-    private void sendToClient(Long id, Object data) {
-        SseEmitter emitter = emitterRepository.get(id);
+    private void sendToClient(Long userId, Object data) {
+        // 유저 ID로 SseEmitter를 찾아 이벤트를 발생 시킨다.
+        SseEmitter emitter = emitterRepository.get(userId);
         if (emitter != null) {
             try {
-                emitter.send(SseEmitter.event().id(String.valueOf(id)).name("sse").data(data));
+                emitter.send(SseEmitter.event().id(String.valueOf(userId)).name("sse").data(data));
             } catch (IOException exception) {
-                emitterRepository.deleteById(id);
+                // IOException이 발생하면 저장된 SseEmitter를 삭제하고 예외를 발생시킨다.
+                emitterRepository.deleteById(userId);
+                log.info("No emitter found");
                 throw new RuntimeException("연결 오류!");
             }
         }
     }
 
-    private SseEmitter createEmitter(Long id) {
+    private SseEmitter createEmitter(Long userId) {
+        // 새로운 SseEmitter를 만든다
         SseEmitter emitter = new SseEmitter(DEFAULT_TIMEOUT);
-        emitterRepository.save(id, emitter);
+        // 유저 ID로 SseEmitter를 저장한다.
+        emitterRepository.save(userId, emitter);
 
         // Emitter가 완료될 때(모든 데이터가 성공적으로 전송된 상태) Emitter를 삭제한다.
-        emitter.onCompletion(() -> emitterRepository.deleteById(id));
+        emitter.onCompletion(() -> emitterRepository.deleteById(userId));
         // Emitter가 타임아웃 되었을 때(지정된 시간동안 어떠한 이벤트도 전송되지 않았을 때) Emitter를 삭제한다.
-        emitter.onTimeout(() -> emitterRepository.deleteById(id));
+        emitter.onTimeout(() -> emitterRepository.deleteById(userId));
 
+        // 503 Service Unavailable 오류가 발생하지 않도록 첫 데이터를 보낸다.
+        try {
+            emitter.send(SseEmitter.event().id("").name(NOTIFICATION_NAME).data("Connection completed"));
+        } catch (IOException exception) {
+            throw new IllegalArgumentException();
+        }
         return emitter;
     }
 
