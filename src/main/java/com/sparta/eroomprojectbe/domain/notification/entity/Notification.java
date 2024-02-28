f7a63c63d8520e69c3ae66fe89db5b23e9bf7c8a
kuma0112 <lsj23871@gmail.com>
2024-02-29 00:24:30 +0900

    chore : 주석 처리

--- src/main/java/com/sparta/eroomprojectbe/domain/notification/entity/Notification.java
+++ src/main/java/com/sparta/eroomprojectbe/domain/notification/entity/Notification.java
@@ -25,22 +25,22 @@ public class Notification {
 
     @Column(nullable = false)
     private Boolean isRead;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private NotificationType notificationType;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    private Member member;
-
-    @Builder
-    public Notification(String content, Long groupId, Boolean isRead, NotificationType notificationType, Member member) {
-        this.content = content;
-        this.groupId = groupId;
-        this.isRead = isRead;
-        this.notificationType = notificationType;
-        this.member = member;
-    }
+//
+//    @Enumerated(EnumType.STRING)
+//    @Column(nullable = false)
+//    private NotificationType notificationType;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    @OnDelete(action = OnDeleteAction.CASCADE)
+//    private Member member;
+//
+//    @Builder
+//    public Notification(String content, Long groupId, Boolean isRead, NotificationType notificationType, Member member) {
+//        this.content = content;
+//        this.groupId = groupId;
+//        this.isRead = isRead;
+//        this.notificationType = notificationType;
+//        this.member = member;
+//    }
 }
