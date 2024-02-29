76e3f0968f817773fb9cebb7d0cd5d4fc34e1d16
kuma0112 <lsj23871@gmail.com>
2024-02-29 23:28:58 +0900

    refactor : 지나친 이벤트에 대한 알람 전송할 수 있도록 리팩토링

--- src/main/java/com/sparta/eroomprojectbe/domain/notification/service/NotificationService.java
+++ src/main/java/com/sparta/eroomprojectbe/domain/notification/service/NotificationService.java
@@ -3,7 +3,9 @@ package com.sparta.eroomprojectbe.domain.notification.service;
 import com.sparta.eroomprojectbe.domain.member.entity.Member;
 import com.sparta.eroomprojectbe.domain.notification.dto.IsReadResponseDto;
 import com.sparta.eroomprojectbe.domain.notification.repository.EmitterRepository;
+import com.sparta.eroomprojectbe.domain.notification.repository.EmitterRepositoryImpl;
 import com.sparta.eroomprojectbe.domain.notification.repository.NotificationRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,94 +13,93 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
-    // 기본 타임아웃 설정
-    private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
-    private final static String NOTIFICATION_NAME = "notify";
-
-    private final EmitterRepository emitterRepository;
+    private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60; // 기본 타임아웃 설정
+    private final EmitterRepositoryImpl emitterRepository;
     private final NotificationRepository notificationRepository;
 
-    public SseEmitter subscribe(Member member, Long userId) {
-        // 새로운 SseEmitter를 만든 후 저장하는 메서드인 createEmitter()를 호출한다.
-        SseEmitter emitter = createEmitter(userId);
+    public SseEmitter subscribe(Member member, String lastEventId, HttpServletResponse response) {
+        String emitterId = makeTimeIncludeId(member);
+        SseEmitter emitter = new SseEmitter(DEFAULT_TIMEOUT);
 
-        sendToClient(userId, "EventStream Created. [userId=" + userId + "]");
+        response.setHeader("X-Accel-Buffering", "no"); // NGINX PROXY 에서의 필요설정 불필요한 버퍼링방지
+
+        emitter.onCompletion(() -> emitterRepository.deleteById(emitterId));
+        emitter.onTimeout(() -> emitterRepository.deleteById(emitterId));
+        emitter.onError((e) -> log.error("SSE Emitter Error: ", e));
+
+        emitterRepository.save(emitterId, emitter);
+
+        // 503 에러를 방지하기 위한 더미 이벤트 전송
+        String eventId = makeTimeIncludeId(member);
+        sendDummyData(eventId, emitter, emitterId, "EventStream Created. [memberId=" + member.getMemberId() + "]");
+
+        // 클라이언트가 미수신한 Event 목록이 존재할 경우 전송하여 Event 유실을 예방
+        resendLostData(lastEventId, member, emitter);
         return emitter;
     }
 
-//    public SseEmitter subscribe(Member member, String lastEventId) {
-//        String emitterId = makeTimeIncludeId(member);
-//
-//        SseEmitter emitter = emitterRepository.save(emitterId, new SseEmitter(DEFAULT_TIMEOUT));
-//
-//        emitter.onCompletion(() -> emitterRepository.deleteById(emitterId));
-//        emitter.onTimeout(() -> emitterRepository.deleteById(emitterId));
-//
-//        String eventId = makeTimeIncludeId(member);
-//        sendDummyData(emitterId, emitter, eventId, "EventStream Created. [memberId="+member.getMemberId()+"]");
-//
-//        if (hasLostData(lastEventId)){
-//            Map<String, Object> events = emitterRepository.findAllEventCacheStartWithByMemberId(String.valueOf(member.getMemberId()));
-//            events.entrySet().stream()
-//                    .filter(entry -> lastEventId.compareTo(entry.getKey()) < 0)
-//                    .forEach(entry -> sendDummyData(emitterId, emitter, entry.getKey(), entry.getValue()));
-//        }
-//        return emitter;
-//    }
-
-    public void notify(Long userId, Object event) {
-        sendToClient(userId, event);
+    public void notify(String emitterId, Object event) {
+        sendToClient(emitterId, event);
     }
 
-    private void sendToClient(Long userId, Object data) {
-        // 유저 ID로 SseEmitter를 찾아 이벤트를 발생 시킨다.
-        SseEmitter emitter = emitterRepository.get(userId);
+    private void sendToClient(String emitterId, Object data) {
+        SseEmitter emitter = emitterRepository.findByEmitterId(emitterId);
         if (emitter != null) {
             try {
-                emitter.send(SseEmitter.event().id(String.valueOf(userId)).name("sse").data(data));
-            } catch (IOException exception) {
+                emitter.send(SseEmitter.event().id(emitterId).name("sse").data(data));
+            } catch (IOException e) {
                 // IOException이 발생하면 저장된 SseEmitter를 삭제하고 예외를 발생시킨다.
-                emitterRepository.deleteById(userId);
-                log.info("No emitter found");
+                emitterRepository.deleteById(emitterId);
+                log.error("SSE 연결 오류 발생", e);
                 throw new RuntimeException("연결 오류!");
             }
         }
     }
 
-    private SseEmitter createEmitter(Long userId) {
-        // 새로운 SseEmitter를 만든다
-        SseEmitter emitter = new SseEmitter(DEFAULT_TIMEOUT);
-        // 유저 ID로 SseEmitter를 저장한다.
-        emitterRepository.save(userId, emitter);
-
-        // Emitter가 완료될 때(모든 데이터가 성공적으로 전송된 상태) Emitter를 삭제한다.
-        emitter.onCompletion(() -> emitterRepository.deleteById(userId));
-        // Emitter가 타임아웃 되었을 때(지정된 시간동안 어떠한 이벤트도 전송되지 않았을 때) Emitter를 삭제한다.
-        emitter.onTimeout(() -> emitterRepository.deleteById(userId));
-
-        // 503 Service Unavailable 오류가 발생하지 않도록 첫 데이터를 보낸다.
-        try {
-            emitter.send(SseEmitter.event().id("").name(NOTIFICATION_NAME).data("Connection completed"));
-        } catch (IOException exception) {
-            throw new IllegalArgumentException();
-        }
-        return emitter;
-    }
-
     @Transactional(readOnly = true)
     public IsReadResponseDto readFindNotification(Member member) {
-        if (notificationRepository.existsByIsReadAndMember(false, member)) {
-            return new IsReadResponseDto(false);
-        }
-        return new IsReadResponseDto(true);
+        boolean isRead = !notificationRepository.existsByIsReadAndMember(false, member);
+        return new IsReadResponseDto(isRead);
     }
 
-//    private String makeTimeIncludeId(Member member) {
-//        return member.getMemberId() + "_" + System.currentTimeMillis();
-//    }
+    private String makeTimeIncludeId(Member member) {
+        return member.getMemberId() + "_" + System.currentTimeMillis();
+    }
+
+    private void resendLostData(String lastEventId, Member member, SseEmitter emitter) {
+        // 놓친 이벤트가 있다면
+        if (!lastEventId.isEmpty()) {
+            // 멈버 아이디를 기준으로 캐시된 모든 이벤트를 가져온다.
+            Map<String, Object> cachedEvents = emitterRepository.findAllEventCacheStartWithByMemberId(String.valueOf(member.getMemberId()));
+
+            // 모든 이벤트를 순회하며
+            for (Map.Entry<String, Object> entry : cachedEvents.entrySet()) {
+                // lastEventId보다 큰 ID(뒷 시간에 일어난 이벤트)만 필터링하여
+                if (lastEventId.compareTo(entry.getKey()) < 0) {
+                    try {
+                        // 재전송한다.
+                        emitter.send(SseEmitter.event().id(entry.getKey()).data(entry.getValue()));
+                    } catch (IOException e) {
+                        log.error("Resending lost data failed for memberId: {}", member.getMemberId(), e);
+                    }
+                }
+            }
+        }
+    }
+
+    private void sendDummyData(String emitterId, SseEmitter emitter, String eventId, Object data) {
+        try {
+            emitter.send(SseEmitter.event()
+                    .id(eventId)
+                    .data(data));
+        } catch (IOException exception) {
+            emitterRepository.deleteById(emitterId);
+        }
+    }
 }
