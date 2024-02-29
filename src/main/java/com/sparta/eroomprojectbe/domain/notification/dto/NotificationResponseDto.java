cfd1f2538595a6fcc84047ac0b6056609000938b
kuma0112 <lsj23871@gmail.com>
2024-03-01 00:50:57 +0900

    refactor : 알림 관련 dto 추가

---
+++ src/main/java/com/sparta/eroomprojectbe/domain/notification/dto/NotificationResponseDto.java
@@ -0,0 +1,27 @@
+package com.sparta.eroomprojectbe.domain.notification.dto;
+
+import com.sparta.eroomprojectbe.domain.notification.entity.Notification;
+import com.sparta.eroomprojectbe.domain.notification.entity.NotificationType;
+import lombok.Getter;
+
+@Getter
+public class NotificationResponseDto {
+    private Long notificationId;
+    private Long challengeId;
+    private Long authId;
+    private NotificationType notificationType;
+    private String content;
+
+    public NotificationResponseDto(Notification notification) {
+        this.notificationId = notification.getId();
+        this.challengeId = notification.getChallengeId();
+        this.authId = notification.getAuthId();
+        this.notificationType = notification.getNotificationType();
+        this.content = notification.getContent();
+    }
+
+    // 정적 팩토리 메서드 추가
+    public static NotificationResponseDto of(Notification notification) {
+        return new NotificationResponseDto(notification);
+    }
+}
