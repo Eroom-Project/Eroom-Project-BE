016156b9faeea39237c19e9ae13b99b8982a7f75
eunchaelyu <hn04133@naver.com>
2024-03-01 23:53:56 +0900

    feature: 채팅기능 구현(선택한 채팅메시지 삭제 기능)

--- src/main/java/com/sparta/eroomprojectbe/domain/chat/controller/ChatController.java
+++ src/main/java/com/sparta/eroomprojectbe/domain/chat/controller/ChatController.java
@@ -1,16 +1,22 @@
 package com.sparta.eroomprojectbe.domain.chat.controller;
 
+import com.sparta.eroomprojectbe.domain.challenge.dto.BaseResponseDto;
 import com.sparta.eroomprojectbe.domain.chat.entity.ChatMessage;
 import com.sparta.eroomprojectbe.domain.chat.service.ChatMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class ChatController {
 
     private final ChatMessageService chatMessageService;
@@ -25,5 +31,17 @@ public class ChatController {
                             @DestinationVariable("challengeId") String challengeId,
                             Message<?> message) {
         chatMessageService.saveMessage(challengeId, chatMessage, message);
+    }
+
+    @DeleteMapping("/api/chat/{challengeId}/{messageNumber}")
+    public ResponseEntity<BaseResponseDto<String>> deleteChatMessage(@PathVariable String challengeId,
+                                                                     @PathVariable Long messageNumber) {
+        boolean deleteSuccess = chatMessageService.deleteChatMessage(challengeId, messageNumber);
+        if (deleteSuccess) {
+            return ResponseEntity.ok().body(new BaseResponseDto<>(null, "채팅 메시지가 성공적으로 삭제되었습니다.", HttpStatus.OK));
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new BaseResponseDto<>(null, "채팅 메시지 삭제에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR));
+        }
     }
 }
