ba0543395409f67e466fab219c6294e94e48ba25
kuma0112 <lsj23871@gmail.com>
2024-03-08 14:35:05 +0900

    chore : 자바독 작성

--- src/main/java/com/sparta/eroomprojectbe/domain/member/controller/MemberController.java
+++ src/main/java/com/sparta/eroomprojectbe/domain/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.sparta.eroomprojectbe.domain.member.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.eroomprojectbe.domain.member.dto.*;
+import com.sparta.eroomprojectbe.domain.member.dto.mypage.MypageResponseDto;
 import com.sparta.eroomprojectbe.domain.member.service.KakaoService;
 import com.sparta.eroomprojectbe.domain.member.service.MemberService;
 import com.sparta.eroomprojectbe.global.jwt.UserDetailsImpl;
@@ -31,6 +32,13 @@ public class MemberController {
         this.kakaoService = kakaoService;
     }
 
+    /**
+     * 회원가입 컨트롤러 메서드
+     *
+     * @param requestDto 회원가입 시 필요한 정보들을 담은 dto
+     * @param bindingResult 유효성 검증을 통과하지 못한 오류들
+     * @return 회원가입한 유저의 정보, 회원가입 성공여부 message, httpStatus
+     */
     @PostMapping("/api/signup")
     public ResponseEntity<BaseDto<SignupResponseDto>> signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -48,32 +56,66 @@ public class MemberController {
         }
     }
 
+    /**
+     * 로그아웃 컨트롤러 메서드
+     *
+     * @param response http 응답 객체
+     * @param refreshToken 유저 확인을 위한 리프레시 토큰
+     * @return 로그아웃 성공여부 message, httpStatus
+     */
     @PostMapping("/api/logout")
     public ResponseEntity<BaseDto<String>> logout(HttpServletResponse response, @CookieValue(name = "Refresh-token") String refreshToken){
         String message = memberService.logout(response, refreshToken);
         return ResponseEntity.ok(new BaseDto<>(null, message, HttpStatus.OK));
     }
 
+    /**
+     * 이메일 중복확인 컨트롤러 메서드
+     *
+     * @param email 유저가 사용하려는 이메일
+     * @return 이메일 중복여부 message, httpStatus
+     */
     @GetMapping("/api/signup/email")
     public ResponseEntity<BaseDto<String>> checkEmail(@RequestParam String email) {
         String message = memberService.checkEmail(email);
         return ResponseEntity.ok(new BaseDto<>(null, message, HttpStatus.OK));
     }
 
+    /**
+     * 닉네임 중복확인 컨트롤러 메서드
+     *
+     * @param nickname 유저가 사용하려는 닉네임
+     * @return 닉네임 중복여부 message, httpStatus
+     */
     @GetMapping("/api/signup/nickname")
     public ResponseEntity<BaseDto<String>> checkNickname(@RequestParam String nickname) {
         String message = memberService.checkNickname(nickname);
         return ResponseEntity.ok(new BaseDto<>(null, message, HttpStatus.OK));
     }
 
-    // 토큰 재발행
+    /**
+     * 토큰 재발급 컨트롤러 메서드
+     *
+     * @param refreshToken access token 재발급을 위한 컨트롤러 메서드
+     * @param response http 응답 객체
+     * @return 토큰 재발급 성공여부 message, httpStatus
+     * @throws UnsupportedEncodingException
+     */
     @PostMapping("/api/token")
-    public ResponseEntity<BaseDto<String>> reissueToken(@CookieValue(name = "Refresh-token") String refreshToken, HttpServletResponse res) throws UnsupportedEncodingException, UnsupportedEncodingException {
-        String message = memberService.reissueToken(refreshToken, res);
+    public ResponseEntity<BaseDto<String>> reissueToken(@CookieValue(name = "Refresh-token") String refreshToken, HttpServletResponse response) throws UnsupportedEncodingException {
+        String message = memberService.reissueToken(refreshToken, response);
         return ResponseEntity.ok(new BaseDto<>(null, message, HttpStatus.OK));
     }
 
-    // 카카오 로그인
+    /**
+     * 카카오 로그인 컨트롤러 메서드
+     *
+     * @param code 카카오 인증 코드
+     * @param response http 응답 객체
+     * @return 로그인 성공여부 message, httpStatus
+     * @throws UnsupportedEncodingException
+     * @throws JsonProcessingException
+     */
     @GetMapping("/auth/callback/kakao")
     public ResponseEntity<BaseDto<String>> kakaoLogin(@RequestParam String code,
                                              HttpServletResponse response) throws UnsupportedEncodingException, JsonProcessingException {
@@ -81,14 +123,25 @@ public class MemberController {
         return ResponseEntity.ok(new BaseDto<>(null, message, HttpStatus.OK));
     }
 
-    // 마이 페이지 조회
+    /**
+     * 마이페이지 조회 컨트롤러 메서드
+     *
+     * @param userDetails 로그인한 유저의 정보
+     * @return MypageResponseDto, httpStatus
+     */
     @GetMapping("/api/mypage")
     public ResponseEntity<BaseDto<MypageResponseDto>> getMypage(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         MypageResponseDto data = memberService.getMypage(userDetails.getMember());
         return ResponseEntity.ok(new BaseDto<>(data, "", HttpStatus.OK));
     }
 
-    // 마이 페이지 닉네임 수정
+    /**
+     * 닉네임 수정 컨트롤러 메서드
+     *
+     * @param nickname 유저가 사용하려는 닉네임
+     * @param userDetails 로그인한 유저의 정보
+     * @return updatedNickname, 닉네임 수정 성공여부 message, httpStatus
+     */
     @PutMapping("/api/member/profile/nickname")
     public ResponseEntity<BaseDto<String>> updateNickname(@RequestParam("profileNickname") String nickname,
                                                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -96,23 +149,41 @@ public class MemberController {
         return ResponseEntity.ok(new BaseDto<>(updatedNickname, "닉네임 수정 성공", HttpStatus.OK));
     }
 
-    // 마이 페이지 프로필 이미지 수정
+    /**
+     * 프로필 이미지 수정 컨트롤러 메서드
+     *
+     * @param file 유저가 사용하려는 프로필 이미지
+     * @param userDetails 로그인한 유저의 정보
+     * @return updatedProfileImage, 프로필 이미지 수정 성공여부 message, httpStatus
+     */
     @PutMapping("/api/member/profile/image")
     public ResponseEntity<BaseDto<String>> updateProfileImage(@RequestParam(value = "profileImageUrl", required = false) MultipartFile file,
                                                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        String updatedNickname = memberService.updateProfileImage(file, userDetails.getMember());
-        return ResponseEntity.ok(new BaseDto<>(updatedNickname, "프로필 이미지 수정 성공", HttpStatus.OK));
+        String updatedProfileImage = memberService.updateProfileImage(file, userDetails.getMember());
+        return ResponseEntity.ok(new BaseDto<>(updatedProfileImage, "프로필 이미지 수정 성공", HttpStatus.OK));
     }
 
-    // 마이 페이지 비밀번호 수정
+    /**
+     * 비밀번호 수정 컨트롤러 메서드
+     *
+     * @param password 유저가 사용하려는 비밀번호
+     * @param userDetails 로그인한 유저의 정보
+     * @return 비밀번호 수정 성공여부 message, httpStatus
+     */
     @PutMapping("/api/member/profile/password")
     public ResponseEntity<BaseDto<String>> updatePassword(@RequestBody Map<String, String> password,
                                                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
         memberService.updatePassword(password.get("password"), userDetails.getMember());
-        return ResponseEntity.ok(new BaseDto<>("", "비밀번호 수정 성공", HttpStatus.OK));
+        return ResponseEntity.ok(new BaseDto<>(null, "비밀번호 수정 성공", HttpStatus.OK));
     }
 
-    // 비밀번호 확인
+    /**
+     * 회원 정보 수정을 위한 비밀번호 확인 컨트롤러 메서드
+     *
+     * @param userDetails 로그인한 유저의 정보
+     * @param password 유저의 현재 비밀번호
+     * @return 비밀번호 일치여부 message, httpStatus
+     */
     @GetMapping("/api/member/password")
     public ResponseEntity<BaseDto<String>> checkPassword(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam String password) {
         boolean isMatch = memberService.checkPassword(userDetails.getMember(), password);
@@ -123,12 +194,25 @@ public class MemberController {
         }
     }
 
+    /**
+     * 인증 코드 요청 컨트롤러 메서드
+     *
+     * @param email 인증 코드를 전송받을 이메일
+     * @return 가입 여부 및 인증코드 전송 message, httpStatus
+     */
     @PostMapping("/emails/verification-requests")
     public ResponseEntity<BaseDto<String>> sendMessage(@RequestParam("email") @Valid String email) {
         String message = memberService.sendCodeToEmail(email);
         return ResponseEntity.ok(new BaseDto<>(null, message, HttpStatus.OK));
     }
 
+    /**
+     * 인증 코드 확인 컨트롤러 메서드
+     *
+     * @param email 인증 코드를 전송받은 이메일
+     * @param authCode 전송받은 인증 코드
+     * @return 인증시간 초과 여부 및 인증 코드 확인 message, httpStatus
+     */
     @GetMapping("/emails/verifications")
     public ResponseEntity<BaseDto<String>> verificationEmail(@RequestParam("email") @Valid String email,
                                                               @RequestParam("code") String authCode) {
